49622443593c7abfc359dab0d6aa36a02f3fbbcc
Perkerti Koirala <78633309+Perkerti69@users.noreply.github.com>
2023-02-19 16:52:24 -0500

    removed comments

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -41,13 +41,6 @@ function moveToPrevSlide() {
   } else {
     slidePosition -= 1;
    }
-  // add your code in here for when you click the "prev" button
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
   updateSlidePosition();
 }
 
